fae6709153aeaefdca99b1130cff745dcb27f017
Gabriel Fernandes <gabrielfernnd@gmail.com>
2020-02-09 17:28:47 -0300

    Progressive Web App

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const fs = require('fs')
 const server = require('http').createServer()
 const io = require('socket.io')(server)
 
-let html
-fs.readFile('index.html', (err, data) => {
-    if (err) throw err
-    html = data
-    server.listen(3000)
-})
+let html = fs.readFileSync('index.html')
+let manifest = fs.readFileSync('manifest.json')
+let icon = fs.readFileSync('icon.png')
+server.listen(process.env.PORT || 3000)
 
 server.on('request', (req, res) => {
     if (req.url == '/' || req.url == '/index.html') {
         res.end(html)
+    } else if (req.url == '/manifest.json') {
+        res.end(manifest)
+    } else if (req.url == '/icon.png') {
+        res.end(icon)
     } else if (!req.url.startsWith('/socket.io')) {
         res.statusCode = 404
         res.end()
